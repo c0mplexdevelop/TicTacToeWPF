d140a8274d20ac0e0171785ec80118a458c42d75
c0mplexdev <105768978+c0mplexdevelop@users.noreply.github.com>
2023-09-06 20:54:25 +0800

    Add project files.

---
+++ MainWindow.xaml.vb
@@ -0,0 +1,30 @@
+﻿Class MainWindow
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Debug.WriteLine("Initialized")
+
+    End Sub
+    Private Sub borderOnClick(sender As Object, ev As MouseButtonEventArgs)
+        'Dim border As Border = TryCast(sender, Border)
+        'Dim row As Integer = CInt(border.GetValue(Grid.RowProperty))
+        'Dim col As Integer = CInt(border.GetValue(Grid.ColumnProperty))
+        'Debug.WriteLine("Clicked4")
+        Debug.WriteLine("Clicky2")
+        Dim border As Border = TryCast(sender, Border)
+
+        ' Retrieve the coordinate of the mouse position.
+        Dim pt As Point = ev.GetPosition(CType(sender, UIElement))
+
+        ' Perform the hit test against a given portion of the visual object tree.
+        Dim result As HitTestResult = VisualTreeHelper.HitTest(border, pt)
+
+        If result IsNot Nothing Then
+            ' Perform action on hit visual object.
+        End If
+    End Sub
+End Class
---
+++ TTT.vb
@@ -0,0 +1,26 @@
+﻿Option Strict On
+
+Imports System.ComponentModel
+
+Public Class TTT
+    Implements INotifyPropertyChanged
+
+    Private currentTurn As String = "X"
+
+    Public Property Turn As String
+        Get
+            Return currentTurn
+        End Get
+        Set(value As String)
+            currentTurn = value
+            NotifyPropertyChanged("Turn")
+        End Set
+    End Property
+
+    Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+
+    Private Sub NotifyPropertyChanged(ByVal info As String)
+        RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(info))
+    End Sub
+
+End Class
