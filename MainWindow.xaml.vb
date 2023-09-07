9931b440dc725c84f941c3ab83a47c1d0a7dc80d
c0mplexdevelop <105768978+c0mplexdevelop@users.noreply.github.com>
2023-09-07 23:35:50 +0800

    Moved borderOnClick to Styles VM

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -9,13 +9,6 @@
         Debug.WriteLine("Initialized")
 
     End Sub
-    Private Sub borderOnClick(sender As Object, ev As MouseButtonEventArgs)
-        'Dim border As Border = TryCast(sender, Border)
-        'Dim row As Integer = CInt(border.GetValue(Grid.RowProperty))
-        'Dim col As Integer = CInt(border.GetValue(Grid.ColumnProperty))
-        'Debug.WriteLine("Clicked4")
-        Debug.WriteLine("Clicky2")
-        Dim border As Border = TryCast(sender, Border)
 
     Private Sub Window_MouseDown(sender As Object, e As MouseButtonEventArgs)
         If (e.ChangedButton = MouseButton.Left) Then
