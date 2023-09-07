1082d6e68eb79d18d6473abb194a4869ac8d338d
c0mplexdevelop <105768978+c0mplexdevelop@users.noreply.github.com>
2023-09-07 23:33:02 +0800

    Added Styles Resource Dictionary and ViewModel (VM for event handling, Dict for styling)

---
+++ Styles.xaml.vb
@@ -0,0 +1,23 @@
+﻿Option Strict On
+
+Public Class Styles
+
+    Private game As TTT
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        game = TryCast(Application.Current.MainWindow.DataContext, TTT)
+    End Sub
+
+    Private Sub borderOnClick(sender As Object, e As MouseButtonEventArgs)
+        Dim border As Border = TryCast(sender, Border)
+        Dim row As Integer = Grid.GetRow(border)
+        Dim col As Integer = Grid.GetColumn(border)
+
+        Debug.WriteLine($"Clicky clackity at {row}-ity {col}-ity")
+    End Sub
+End Class
