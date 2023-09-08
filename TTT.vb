13a873516c11d0ae89bfc09126f2eccb1608ed6b
c0mplexdev <105768978+c0mplexdevelop@users.noreply.github.com>
2023-09-08 13:45:44 +0800

    Added switch turns and checking if a someone won functionality.

--- TTT.vb
+++ TTT.vb
@@ -38,4 +38,42 @@ Public Class TTT
     Public Sub SetPieceToPosition(row As Integer, col As Integer)
         board(row, col) = currentTurn
     End Sub
+
+    Public Sub SwitchTurns()
+        Turn = If(Turn = "X", "O", "X")
+    End Sub
+
+    Public Function CheckIfLetterWon() As Boolean
+        Return CheckIfRowWon() OrElse CheckIfColWon() OrElse CheckIfLeftDiagonalWon() OrElse CheckIfRightDiagonalWon()
+    End Function
+
+    Private Function CheckIfRowWon() As Boolean
+        For row = 0 To board.GetUpperBound(0)
+            If board(row, 0) <> EMPTY_SPACE AndAlso board(row, 0) = board(row, 1) AndAlso board(row, 1) = board(row, 2) Then
+                Return True
+            End If
+        Next
+
+        Return False
+    End Function
+
+    Private Function CheckIfColWon() As Boolean
+        For col = 0 To board.GetUpperBound(0)
+            If board(0, col) <> EMPTY_SPACE AndAlso board(0, col) = board(1, col) AndAlso board(1, col) = board(2, col) Then
+                Return True
+            End If
+        Next
+
+        Return False
+    End Function
+
+
+    ' Diagonals are based from the most bottom part of the board
+    Private Function CheckIfLeftDiagonalWon() As Boolean
+        Return board(0, 0) <> EMPTY_SPACE AndAlso board(0, 0) = board(1, 1) AndAlso board(1, 1) = board(2, 2)
+    End Function
+
+    Private Function CheckIfRightDiagonalWon() As Boolean
+        Return board(0, 2) <> EMPTY_SPACE AndAlso board(0, 2) = board(1, 1) AndAlso board(1, 1) = board(2, 0)
+    End Function
 End Class
